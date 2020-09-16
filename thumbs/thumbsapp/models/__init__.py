2b52a5637299ba2bcab197d7deda738c0687743e
iamkaleb <kalebdmoran@gmail.com>
2020-09-16 10:20:57 -0500

    Put models in the right place and added init py

---
+++ thumbs/thumbsapp/models/__init__.py
@@ -0,0 +1,7 @@
+from .group import Group
+from .group_user import GroupUser
+from .idea import Idea
+from .idea_image import IdeaImage
+from .idea_video import IdeaVideo
+from .poll import Poll
+from .vote import Vote
